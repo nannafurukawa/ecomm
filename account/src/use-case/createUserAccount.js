7bde665301cf71bc4bffde8051da12732ad27f26
Nanna Bessa <105128170+nannafurukawa@users.noreply.github.com>
2022-12-02 13:18:06 -0300

    task 1: criar casode uso de conta e seus testes

--- account/src/use-case/createUserAccount.js
+++ account/src/use-case/createUserAccount.js
@@ -1,29 +1,19 @@
-let contas = []
+const users = [];
 
-export function createUserUseCase(nome, email, senha) {
+export function createUserUseCase(name, email, password) {
+const id = users.length + 1;
+const createdDate = new Date(). toISOString().substring(0, 10);
 
-    const user = {
-        id: contas.length + 1,
-        name: nome,
-        email: email,
-        password: senha,
-        createdDate: (formatDate(new Date()))
-    }
+const user = {
+    id,
+    name,
+    email,
+    password,
+    createdDate
+};
 
-    contas.push(user)
-    return user
+users.push(user);
+
+return user;
+
 }
-function formatDate(date) {
-    return [
-        padTo2Digits(date.getDate()),
-        padTo2Digits(date.getMonth() + 1),
-        date.getFullYear(),
-    ].join('/');
-}
-function padTo2Digits(num) {
-    return num.toString().padStart(2, '0');
-}
-createUserUseCase("nanna", 'nannabessa@gmail.com', '12345')
-createUserUseCase("marlon", 'lobofurukawa@yahoo.com', '12345')
-
-console.log("contas:", contas)
---
+++ test/use-case/createUserAccount.test.js
@@ -0,0 +1,6 @@
+import {createUserUseCase} from '../../account/src/use-case/createUserAccount.js';
+
+const user1 = createUserUseCase('Nanna', 'nannabessa@gmail.com', 'nanna07');
+const user2= createUserUseCase('Marlon', 'lobofurukawa@yahoo.com', 'marlonlobo');
+
+console.log(user1, user2);
