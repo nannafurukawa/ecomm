af5e5241cfdd9524e991af3be8709675d289f3bf
Rafael Lima <n665030@gruposantander.com>
2023-01-06 18:02:16 -0300

    update docs

--- product/src/main.js
+++ product/src/main.js
@@ -1,7 +1,24 @@
 import express from 'express';
-
 import { router } from './routes.js';
-const port = 3000;
+import { Sequelize } from 'sequelize';
+
+const sequelize = new Sequelize(
+   'db',
+   'db',
+   'dbpw',
+    {
+      host: '0.0.0.0:3006',
+      dialect: 'mysql'
+    }
+  );
+
+sequelize.authenticate().then(() => {
+   console.log('Connection has been established successfully.');
+}).catch((error) => {
+   console.error('Unable to connect to the database: ', error);
+});
+
+const port = 3001;
 const app = express();
 app.use(express.json());
 app.use(router);
